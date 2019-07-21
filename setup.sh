e8447a290631ee118e5852b5609f4327baa11a17
Alejandro Coronado <acoronadn@gmail.com>
2019-07-21 08:17:53 -0500

    changes in setup, use python instead of python3

--- setup.sh
+++ setup.sh
@@ -1,6 +1,6 @@
 
 #!/usr/bin/env bash
-
+sudo apt-get install python3-pip
 pip install -r requirements.txt 
 #InstagramAPI
 pip install git+git://github.com/coronate/Instagram-API-python.git
@@ -8,8 +8,9 @@ pip install git+git://github.com/coronate/Instagram-API-python.git
 #NordVPN install
 sudo apt install wget apt-transport-https
 wget --directory-prefix /tmp https://repo.nordvpn.com/deb/nordvpn/debian/pool/main/nordvpn-release_1.0.0_all.deb
-sudo apt install /tmp/nordvpn-release_1.0.0_all.deb
+sudo apt-get install /tmp/nordvpn-release_1.0.0_all.deb
+sudo apt-get update
+sudo apt-get install nordvpn
 sudo apt-get update
 
-python3 filesystem_instagram_dropbox.py
-
+nordvpn login
